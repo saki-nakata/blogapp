9e01bb01fa86579f634f346d1f4905ad379a1ffa
Saki <amethyst225crystal@gmail.com>
2025-08-13 04:12:04 +0900

    Webサイトのデザインを整えよう

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-    def index
-       @article = Article.first
-    end
-    def about
-        @message="aboutページ"
-        render "home/about"
-    end
-end
