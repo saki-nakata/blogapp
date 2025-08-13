2f2908631ad8819b90d9773327a112f984bc37c5
Saki <amethyst225crystal@gmail.com>
2025-08-13 10:12:25 +0900

    CRUD機能を作ってみよう ー 記事の表示①

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+9.times do
+Article.create({title:Faker::Lorem.sentence(word_count: 5), content:Faker::Lorem.sentence(word_count: 50)})
+end
