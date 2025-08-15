e85350dc58516bea5d0287bc52c6c05f0d9b2180
Saki <amethyst225crystal@gmail.com>
2025-08-15 19:16:34 +0900

    CRUD機能を作ってみよう ー Railsの様々な機能を使ってみよう

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  def display_created_at
+    I18n.l(self.created_at, format: :long)
+  end
 end
