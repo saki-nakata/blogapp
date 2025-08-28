3938d89437b6b8595f16852b1f71a42f2fae3704
Saki <amethyst225crystal@gmail.com>
2025-08-28 16:20:53 +0900

    記事に「いいね」しよう + 記事にアイキャッチを設定しよう

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_articles, through: :likes, source: :article
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
 	  articles.exists?(id: article.id)
+  end
+
+  def has_liked?(article)
+    likes.exists?(article_id:article.id)
   end
 
   def get_name
